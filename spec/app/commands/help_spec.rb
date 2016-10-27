7a1d3836affb4732bb10222b42168e057bedf89e
Edward Withers <edward.withers@surfdome.com>
2016-10-27 22:56:31 +0100

    remove old spec, update help and newimage spec

--- spec/app/commands/help_spec.rb
+++ spec/app/commands/help_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe Commands::Help do
-  let(:input) { '?' }
-
-  subject { described_class.new(input) }
-
-  describe "#process" do
-    it "returns message" do
-      expect(subject.process[:message]).to eq "? - Help
+  let(:image) { instance_double("Image") }
+  let(:input) { double(:command) }
+  let(:message) do
+    "? - Help
 I M N - Create a new M x N image with all pixels coloured white (O).
 C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
@@ -15,20 +12,13 @@ V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 an
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 S - Show the contents of the current image
 X - Terminate the session"
-    end
   end
 
-  describe "#valid" do
-    context "invalid input" do
-    let(:input) { '?extra characters' }
-      it "receives invalid input" do
-        expect(subject).to_not be_valid
-      end
-    end
-    context "valid input" do
-      it "is valid" do
-        expect(subject).to be_valid
-      end
+  subject { described_class.new(input, image) }
+
+  describe "#process" do
+    it "returns message" do
+      expect { subject.process }.to output("#{message}\n").to_stdout
     end
   end
 end
--- spec/app/commands/new_image_spec.rb
+++ spec/app/commands/new_image_spec.rb
@@ -1,30 +1,31 @@
 require 'spec_helper'
 
 RSpec.describe Commands::NewImage do
+  let(:image) { instance_double("Image") }
+  let(:input) { [x, y] }
+  subject { described_class.new(*input, image) }
 
-  subject { described_class.new(input) }
-
-  context "valid input" do
-    let(:input) { 'I 3 4' }
-    it "#valid? returns true" do
-      expect(subject).to be_valid
-    end
-    it "returns image" do
-      expect(subject.process[:image]).to eq [%w(O O O), %w(O O O), %w(O O O), %w(O O O )]
+  describe "#required_args" do
+    it "has 2" do
+      expect(described_class.required_args).to eq 2
     end
   end
+  describe "#process" do
+    context "valid input" do
+      let(:x) { '4' }
+      let(:y) { '4' }
 
-  context "invalid input" do
-    context "missing coordinates" do
-      let(:input) { 'I 24' }
-      it "#valid? returns false" do
-        expect(subject).to_not be_valid
+      it "image receives message" do
+        expect(image).to receive(:new_image).with(x.to_i, y.to_i)
+        subject.process
       end
     end
-    context "too many coordinates" do
-      let(:input) { 'I 24 100 2' }
-      it "#valid? returns false" do
-        expect(subject).to_not be_valid
+    context "invalid input" do
+      let(:x) { '300' }
+      let(:y) { '4' }
+
+      it "command raises error" do
+        expect { subject.process }.to raise_error(InvalidCoordinate)
       end
     end
   end
