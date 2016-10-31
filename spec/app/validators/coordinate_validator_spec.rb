cd25a6d0a9b293b89d4a32ce4f939cb3efa2b2a5
Edward Withers <edward.withers@surfdome.com>
2016-10-31 23:09:06 +0000

    non digit character edge case

--- spec/app/validators/coordinate_validator_spec.rb
+++ spec/app/validators/coordinate_validator_spec.rb
@@ -21,6 +21,14 @@ RSpec.describe CoordinateValidator do
         expect(subject).to_not be_valid
       end
     end
+    context "coordinates include non digit characters" do
+      let(:x) { '24t' }
+      let(:y) { '52' }
+
+      it "#valid? returns false" do
+        expect(subject).to_not be_valid
+      end
+    end
     context "x is > 250" do
       let(:x) { '252' }
       let(:y) { '4' }
