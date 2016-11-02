3e4fde3bc33f3b244b1ee791b0b7f897caffb433
Edward Withers <edward.withers@surfdome.com>
2016-11-02 15:23:23 +0000

    use rspec output matcher

--- spec/app/bitmap_editor_spec.rb
+++ spec/app/bitmap_editor_spec.rb
@@ -9,10 +9,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ['t', 'X'] }
 
     it "user input ('t') is shown unrecognised" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with("unrecognised command 't' :(")
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> unrecognised command 't' :(\n> goodbye!\n").to_stdout
     end
   end
 
@@ -20,9 +17,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ['X'] }
 
     it "user input ('X') is shown goodbye" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> goodbye!\n").to_stdout
     end
   end
 
@@ -30,9 +25,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 4", 'X'] }
 
     it "does not output to STDOUT" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > goodbye!\n").to_stdout
     end
   end
 
@@ -40,9 +33,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 3", "L 3 3 P", 'X'] }
 
     it "does not output to STDOUT" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > > goodbye!\n").to_stdout
     end
   end
 
@@ -50,9 +41,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 3", "H 3 3 1 P", 'X'] }
 
     it "does not output to STDOUT" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > > goodbye!\n").to_stdout
     end
   end
 
@@ -60,9 +49,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 3", "V 1 3 3 P", 'X'] }
 
     it "does not output to STDOUT" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > > goodbye!\n").to_stdout
     end
   end
 
@@ -70,9 +57,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 3", "C", 'X'] }
 
     it "does not output to STDOUT" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > > goodbye!\n").to_stdout
     end
   end
 
@@ -80,10 +65,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ["I 3 3", "S", 'X'] }
 
     it "use input('S') is shown image" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with(%w(OOO OOO OOO))
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> > OOO\nOOO\nOOO\n> goodbye!\n").to_stdout
     end
   end
 
@@ -91,17 +73,7 @@ RSpec.describe BitmapEditor do
     let(:input) { ['?', 'X'] }
 
     it "user input ('?') is shown command help" do
-      expect(STDOUT).to receive(:puts).with('type ? for help')
-      expect(STDOUT).to receive(:puts).with("? - Help
-I M N - Create a new M x N image with all pixels coloured white (O).
-C - Clears the table, setting all pixels to white (O).
-L X Y C - Colours the pixel (X,Y) with colour C.
-V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image
-X - Terminate the session")
-      expect(STDOUT).to receive(:puts).with('goodbye!')
-      bitmap_editor.run
+      expect { bitmap_editor.run }.to output("type ? for help\n> ? - Help\nI M N - Create a new M x N image with all pixels coloured white (O).\nC - Clears the table, setting all pixels to white (O).\nL X Y C - Colours the pixel (X,Y) with colour C.\nV X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).\nH X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).\nS - Show the contents of the current image\nX - Terminate the session\n> goodbye!\n").to_stdout
     end
   end
 end
